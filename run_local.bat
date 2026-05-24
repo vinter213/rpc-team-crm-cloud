9bccfa464c8df8d3a0a4add243f8d1b4700c552c
vinter213 <daniel134745@gmail.com>
2026-05-24 06:32:26 +0500

    Delete run_local.bat

--- run_local.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d %~dp0
-py -m venv .venv
-call .venv\Scripts\activate.bat
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-uvicorn main:app --reload --host 127.0.0.1 --port 8000
-pause
